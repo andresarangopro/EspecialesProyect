4d6363061c8bb5392a75dfd64cec1040e11e4aba
Sebassllr <sebaslunasllr@gmail.com>
2017-04-30 14:23:50 -0500

    Cambios clase Operaciones

--- src/mundo/Operaciones.java
+++ src/mundo/Operaciones.java
@@ -1,6 +1,9 @@
 package mundo;
 
 import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
 
 /**
  * Clase encargada de realizar las operaciones entre conjuntos 
@@ -17,14 +20,41 @@ public class Operaciones {
 	 * Arreglo de las relaciones B.
 	 */
 	ArrayList<Relacion> arrayB;
+
+	
+	ArrayList<Relacion> arrayUniversal;
 	
 	public Operaciones(ArrayList<Relacion> arrayA, ArrayList<Relacion> arrayB) {
 		this.arrayA = arrayA;
 		this.arrayB = arrayB;
+		
+		generarUniversal(arrayA, arrayB);
+		
 	}
+
+	private void generarUniversal(ArrayList<Relacion> arrayA2, ArrayList<Relacion> arrayB2) {
+		
+		for (Relacion relacion : arrayA2) {
+			arrayUniversal.add(relacion);
+		}
+		
+		for (Relacion relacion : arrayB2) {
+			arrayUniversal.add(relacion);
+		}
+		
+		Set<Relacion> hs = new HashSet<>();
+		
+		hs.addAll(arrayUniversal);
+		arrayUniversal.clear();
+		arrayUniversal.addAll(hs);
+	}
+	
+
 	
 	//////////////////////////////////////
 	////Getters y Setters
 	//////////////////////////////////////
+	
+	
 	
 }
